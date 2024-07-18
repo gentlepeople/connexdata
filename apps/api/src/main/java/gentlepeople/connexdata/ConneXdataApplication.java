cf0af56fdaa68638fa7031a4b372c41cdfe81c26
2hwayoung <69628269+2hwayoung@users.noreply.github.com>
2024-07-18 17:51:12 +0900

    Refactor/rename connexdata (#2)
    * refactor: rename sheetbridge to connexdata
    
    * refactor: rename sheetbridge to connexdata

---
+++ apps/api/src/main/java/gentlepeople/connexdata/ConneXdataApplication.java
@@ -0,0 +1,14 @@
+package gentlepeople.connexdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConneXdataApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(ConneXdataApplication.class, args);
+  }
+
+}
---
+++ apps/api/src/main/java/gentlepeople/connexdata/CustomerRepository.java
@@ -0,0 +1,21 @@
+package gentlepeople.connexdata;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomerRepository {
+
+  @PersistenceContext
+  private EntityManager em;
+
+  public Long save(Customer customer) {
+    em.persist(customer);
+    return customer.getId();
+  }
+
+  public Customer findById(Long id) {
+    return em.find(Customer.class, id);
+  }
+}
